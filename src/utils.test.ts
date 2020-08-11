3f10aedd766853140965d710ede66df366005f57
anahit <anahit_vardevanyan@ultralabs.io>
2020-08-11 15:26:23 +0400

    test for createCSSClass

--- src/utils.test.ts
+++ src/utils.test.ts
@@ -2,5 +2,7 @@ import {createCSSClass} from "./utils";
 
 
 test('to get string of classes', () => {
-    expect(createCSSClass(['class1', 'class2 '], {"class4 ": false, "class5 ": true})).toBe("class1 class2 class4");
+    expect(createCSSClass(['class1', 'class2 '], {"class4 ": false, "class5 ": true})).toBe("class1 class2 class5");
+    expect(createCSSClass(['class1', 'class2 '], {"class4 ": false, "class5 ": undefined})).toBe("class1 class2");
 });
+
